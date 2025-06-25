ed1aa8d703f630f94f5b8ebf294ee2ab9a37fc20
CelloSerenity <195480169+CelloSerenity@users.noreply.github.com>
2025-06-24 20:22:39 -0600

    Add Antrag to list of apps

--- src/main.rs
+++ src/main.rs
@@ -40,6 +40,7 @@ fn main() {
     supported_apps.insert("Feather".to_string(), "pairingFile.plist".to_string());
     supported_apps.insert("StikDebug".to_string(), "pairingFile.plist".to_string());
     supported_apps.insert("Protokolle".to_string(), "pairingFile.plist".to_string());
+    supported_apps.insert("Antrag".to_string(), "pairingFile.plist".to_string());
 
     let app = MyApp {
         devices: None,
